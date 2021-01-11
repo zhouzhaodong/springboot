73a557572985ec30a24b8126a01ca228b3f28f7f
周兆东 <xiuaiba@163.com>
2021-01-11 13:59:13 +0800

    springboot实现二维码生成

---
+++ spring-boot-qrcode/src/main/java/xyz/zhouzhaodong/www/qrcode/QrcodeApplication.java
@@ -0,0 +1,13 @@
+package xyz.zhouzhaodong.www.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeApplication.class, args);
+    }
+
+}
